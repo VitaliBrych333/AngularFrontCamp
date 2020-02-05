b26c31c31bbbc81a43fcee28aeff83803e0365a0
Vitali Brych <epamc@mail.ru>
2020-02-06 02:13:59 +0300

    update: update

--- server/server.js
+++ server/server.js
@@ -1,37 +1,54 @@
-const express = require('express');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const mongoose = require('mongoose');
 
-const router = require('./routers/router');
-const app = express();
-const path = require('path');
+// const router = require('./routers/router');
+// const app = express();
+// const path = require('path');
 
-mongoose.connect(
-    'mongodb://admin:admin2020@ds135061.mlab.com:35061/news',
-    { useNewUrlParser: true, useFindAndModify: false},
-);
+// mongoose.connect(
+//     'mongodb://admin:admin2020@ds135061.mlab.com:35061/news',
+//     { useNewUrlParser: true, useFindAndModify: false},
+// );
 
-app.set('port', (process.env.PORT || 5500));
-app.use(express.static(path.join(__dirname, '../dist/ang-front-camp')));
-// app.use(express.static(path.join(__dirname + '/dist/ang-front-camp')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.set('port', (process.env.PORT || 5500));
+// app.use(express.static(path.join(__dirname, '../dist/ang-front-camp')));
+// // app.use(express.static(path.join(__dirname + '/dist/ang-front-camp')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
-
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname,'/dist/ang-front-camp/index.html'));
-});
-// app.use('/', router);
-
-// app.use((err, req, res, next) => {
-//     res.status(500).render('error', {contentError: `Error: ${err.message}`})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
 // });
 
-app.listen(app.get('port'), () => {
-    console.log('Node app is running at localhost:' + app.get('port'));
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname,'/dist/ang-front-camp/index.html'));
+// });
+// // app.use('/', router);
+
+// // app.use((err, req, res, next) => {
+// //     res.status(500).render('error', {contentError: `Error: ${err.message}`})
+// // });
+
+// app.listen(app.get('port'), () => {
+//     console.log('Node app is running at localhost:' + app.get('port'));
+// });
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/ang-front-camp'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname,'/dist/ang-front-camp/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
