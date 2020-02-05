09264f3ca2e1c98a2c87025fe65cd85c25d58bc3
Vitali Brych <epamc@mail.ru>
2020-02-06 02:48:31 +0300

    update: update

--- server/server.js
+++ server/server.js
@@ -51,4 +51,4 @@ res.sendFile(path.join(__dirname,'/dist/ang-front-camp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT);
