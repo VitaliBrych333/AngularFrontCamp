d9bfe04e0c8d5f1d93bc61c407a94213dae65651
Vitali Brych <epamc@mail.ru>
2020-02-06 03:16:40 +0300

    update: update

--- server/server.js
+++ server/server.js
@@ -43,12 +43,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angfrontcamp'));
+app.use(express.static('/dist/angfrontcamp'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname,'/dist/angfrontcamp/index.html'));
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, '/dist/angfrontcamp', 'index.html'));
 });
 
+// app.get('/*', function(req,res) {
+
+// res.sendFile(path.join(__dirname,'/dist/angfrontcamp/index.html'));
+// });
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 5500);
