7f4eb90460eccb73fa74a46fc85d8ede20c7365e
Vitali Brych <vbrych@cpaglobal.com>
2020-02-06 17:06:32 +0300

    update: change

--- server/server.js
+++ server/server.js
@@ -1,3 +1,43 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+// const router = require('./routers/router');
+// const app = express();
+// const path = require('path');
+
+// mongoose.connect(
+//     'mongodb://admin:admin2020@ds135061.mlab.com:35061/news',
+//     { useNewUrlParser: true, useFindAndModify: false},
+// );
+
+// app.set('port', (process.env.PORT || 5500));
+
+// app.use(express.static(path.join(__dirname, '../dist/angfrontcamp')));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+// });
+
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname, '../dist/angfrontcamp/index.html'));
+// });
+
+// app.use('/', router);
+
+// app.use((err, req, res, next) => {
+//     res.status(500).render('error', { contentError: `Error: ${err.message}` })
+// });
+
+// app.listen(app.get('port'), () => {
+//     console.log('Node app is running at localhost:' + app.get('port'));
+// });
+
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -31,7 +71,7 @@ app.get('/*', function(req,res) {
 app.use('/', router);
 
 app.use((err, req, res, next) => {
-    res.status(500).render('error', { contentError: `Error: ${err.message}` })
+    res.status(500).render('error', {contentError: `Error: ${err.message}`})
 });
 
 app.listen(app.get('port'), () => {
